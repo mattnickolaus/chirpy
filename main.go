c44d0d6557a62225cdccad4dedb4495723735567
mattnickolaus <mattnickolaus@gmail.com>
2025-12-03 16:50:08 -0500

    settig file server

--- main.go
+++ main.go
@@ -7,14 +7,16 @@ import (
 
 func main() {
 	const port = "8080"
+	const filePath = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filePath)))
 
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
 
-	log.Printf("Listening...\n")
+	log.Printf("Servering file at '%s' on port %s ...\n", filePath, port)
 	log.Fatal(http.ListenAndServe(server.Addr, server.Handler))
 }
