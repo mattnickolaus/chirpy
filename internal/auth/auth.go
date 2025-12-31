d5ede53276fafb965d4e235cd6e6d4741671d246
mattnickolaus <mattnickolaus@gmail.com>
2025-12-30 20:51:14 -0500

    starting to add JWT

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"time"
+
 	"github.com/alexedwards/argon2id"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -20,4 +25,21 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 	}
 
 	return match, nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	issueAt := time.Now().UTC()
+	expiresAt := issueAt.Add(expiresIn)
+
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{
+			Issuer:    "chirpy",
+			IssuedAt:  jwt.NewNumericDate(issueAt),
+			ExpiresAt: jwt.NewNumericDate(expiresAt),
+			Subject:   userID.String(),
+		},
+	)
+
+	token.SignedString()
 }
